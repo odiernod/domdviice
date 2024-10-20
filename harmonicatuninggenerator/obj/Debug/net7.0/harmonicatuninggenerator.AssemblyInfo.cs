0155f5188995d0f5afff4e6993831469b98b2279
Dom <odiernod@gmail.com>
2024-10-20 00:23:43 -0500

    added fourth instrument bank

--- harmonicatuninggenerator/obj/Debug/net7.0/harmonicatuninggenerator.AssemblyInfo.cs
+++ harmonicatuninggenerator/obj/Debug/net7.0/harmonicatuninggenerator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("harmonicatuninggenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed30496ec0f4444ef741dbea91b2eca38db86a77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e51eeb4f354be0d43cd3cc8a1d446834b4e4d7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("harmonicatuninggenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("harmonicatuninggenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
